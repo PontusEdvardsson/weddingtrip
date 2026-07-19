9e85348798e2b5fda63b3abc40f32f5f56321934
PontusEdvardsson <pontuscvedvardsson@gmail.com>
2026-07-19 13:46:57 +0200

    Refine golf packing items

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "brollopsresan-v24";
+const CACHE = "brollopsresan-v25";
 const FILES = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
 self.addEventListener("install", event => {
   self.skipWaiting();
